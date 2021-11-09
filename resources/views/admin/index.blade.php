40823af6daa651437ce0b1a497a57ad603bc985a
SalsabilaDaranti <salsabila_190@smkassalaambandung.sch.id>
2021-11-09 18:43:44 +0700

    Laratrust

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.admin')
+@section ('content')
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    Admin Dashboard
+</div>
+<div class="card-body">
+    Welcome back <strong>Admin</strong>
+</div>
+</div>
+</div>
+</div>
+</div>
+@endsection
